8c57ba1a26b1c57cb2ce26d12a1bb3f4f76370c8
michal <ml212119416@gmail.com>
2021-11-20 23:12:26 +0200

    first commit

---
+++ proj/src/components/AddClock.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+// import useState from 'usestate';
+const AddClock = () => {
+    const [comp, setComp] = useState("aaaaaaaaaaaaaaaaaa");
+    const [mod, setMod] = useState(null);
+    const [col, setCol] = useState(null);
+    const [price, setPrice] = useState(null);
+    const [typ, setTyp] = useState(null);
+    const DoCancel = () => {
+        setComp("");
+        setMod("");
+        setCol("");
+        setPrice("");
+        setTyp("");
+    }
+    return (
+        <>
+            <label>Company Name</label>
+            <input type="text" value={comp} />
+            <br></br>
+            <label>Model</label>
+            <input type="text" value={mod} />
+            <br></br>
+            <label>Color</label>
+            <input type="text" value={col} />
+            <br></br>
+            <label>Price</label>
+            <input type="text" value={price} />
+            <br></br>
+            <label>Type</label>
+            <input type="text" value={typ} />
+            <br></br>
+            <button onClick={()=>{DoCancel}}  >Cancel</button>
+            <button  >Save</button>
+        </>
+    )
+}
+
+export default AddClock;
---
+++ proj/src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AddClock from './AddClock';
+function App() {
+
+  return (
+    <div className="App">
+     <AddClock></AddClock>
+    </div>
+  );
+}
+
+export default App;
